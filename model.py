241b1d80ca016e54af8f3aaedb7fcd7dc8278648
mushtaq2314 <mohammedd.mushtaq@gmail.com>
2023-07-02 00:04:41 +0530

    Model

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
-import sys
-import json
-import ast
-data_to_pass_back ='Send this to node js';
-input =ast.literal_eval(sys.argv[1])
-output = input + data_to_pass_back
-print(json.dumps(output))
-sys.stdout.flush()
+import numpy as np
+import joblib
+model=joblib.load('C:/Users/DELL/Desktop/Indian Railways/model.pkl')
+#[age,gender,loc,finance,occu,passengers,place]
+#gender- F-0 M-1 O-2
+#Finance- Moderate-0 unstable-1 stable-2
+#location - any state 0-29
+#occu- 0-629
+#place- rural-0 urban-1
+print((model.predict(np.array([72,1,26,2,89,2,1]).reshape(1,-1))[0]))
