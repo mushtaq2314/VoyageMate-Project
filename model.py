a6788025a70d281aecf448536555908d6b61b86a
mushtaq2314 <mohammedd.mushtaq@gmail.com>
2023-07-02 00:07:00 +0530

    new

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ import joblib
 model=joblib.load('C:/Users/DELL/Desktop/Indian Railways/model.pkl')
 #[age,gender,loc,finance,occu,passengers,place]
 #gender- F-0 M-1 O-2
-#Finance- Moderate-0 unstable-1 stable-2
 #location - any state 0-29
+#Finance- Moderate-0 unstable-1 stable-2
 #occu- 0-629
 #place- rural-0 urban-1
-print((model.predict(np.array([72,1,26,2,89,2,1]).reshape(1,-1))[0]))
+print((model.predict(np.array([19,1,26,2,89,1,1]).reshape(1,-1))[0]))
